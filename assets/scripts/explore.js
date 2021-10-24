04ca3833eee61a23bf0567bb5b165c85ce428276
farnaf <fnafarif@ucsd.edu>
2021-10-24 15:56:53 -0700

    update explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,20 +1,52 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
-const synth = window.speechSynthesis;
-var select = document.getElementById('voice-select');
-let arr;
 
-setTimeout(() => {
-  arr = synth.getVoices();
-  console.log(arr);
-  for (var i = 0; i <= arr.length; i++){
-    var opt = document.createElement('option');
-    opt.value = arr[i].name;
-    opt.innerHTML = arr[i].name; // show the option
-    select.appendChild(opt);
-  }
-}, 50)
+var synth, voices, utterance;
+var text = '';
 
 function init() {
+  loadVoices();
+
+  let inputText = document.getElementById('text-to-speak');
+  let button = document.querySelector('button');
+  let img = document.querySelector('img');
+
+  inputText.addEventListener('input', function(){
+    text = inputText.value;
+  });
+
+  button.addEventListener('click', function(){
+    utterance = new SpeechSynthesisUtterance(text);
+    let selectedOption = document.getElementById('voice-select').selectedOptions[0].getAttribute('data-name');
+
+    for(var i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        utterance.voice = voices[i];
+      }
+    }
+
+    img.src = 'assets/images/smiling-open.png';
+    synth.speak(utterance);
+    utterance.addEventListener('end', () => {
+      img.src = 'assets/images/smiling.png';
+    });
+  });
+}
+
+
+function loadVoices(){
+  synth = window.speechSynthesis;
+
+  setTimeout(() => {
+    voices = synth.getVoices();
+
+    for (var i = 0; i < voices.length; i++){
+      var opt = document.createElement('option');
+      opt.textContent = voices[i].name + ' (' + voices[i].lang + ' )';
+      opt.setAttribute('data-name', voices[i].name);
+
+      document.getElementById('voice-select').appendChild(opt);
+    }
+  }, 50);
 }
